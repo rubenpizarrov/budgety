4f17719674bf41d8565cff6840146dd4fd07ea3d
Ruben Pizarro <kurai.vk@gmail.com>
2017-08-29 01:33:57 -0300

    getting input values

--- app.js
+++ app.js
@@ -42,16 +42,38 @@ var budgetController = (function(){
     
 //UI CONTROLLER    
 var UIController = (function (){
-        //Some Code
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    return {
+            getInput: function () {
+                return {
+                    type: document.querySelector(DOMstrings.inputType).value,
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value
+                };
+            },
+            getDOMstrings: function() {
+                return DOMstrings;
+            }
+    };
 })();
     
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
     
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function() {
         //TODO
         //1. Obtener los valores de los inputs
-
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. Agregar un Item al BudgetController
 
         //3. Agregar el Item a la UI
@@ -61,12 +83,14 @@ var controller = (function(budgetCtrl, UICtrl){
         //5. Mostrar el budget en la UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();            
         }
+
+
     });
         
 })(budgetController, UIController); 
